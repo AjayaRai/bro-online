b2e33a47261bb9e078c4491e4be7f600386a137a
Ajaya Rai <ar17052@essex.ac.uk>
2020-10-23 14:38:01 +0100

    Social media tutorial; Time on Youtube '7:17:19'

--- Challenge Week/Project/socialape-functions/functions/index.js
+++ Challenge Week/Project/socialape-functions/functions/index.js
@@ -1,16 +1,38 @@
 const functions = require('firebase-functions');
-const express = require('express');
-const app = express();
+const app = require('express')();
 const FBAuth = require('./util/fbAuth');
-const {db} = require('./util/admin');
 
-const {getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream} = require('./handlers/screams');
-const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser, getUserDetails, markNotificationsRead} = require('./handlers/users');
+const cors = require('cors');
+app.use(cors());
 
-// screams route
+const { db } = require('./util/admin');
+
+const {
+    getAllScreams,
+    postOneScream,
+    getScream,
+    commentOnScream,
+    likeScream,
+    unlikeScream,
+    deleteScream
+} = require('./handlers/screams');
+const {
+    signup,
+    login,
+    uploadImage,
+    addUserDetails,
+    getAuthenticatedUser,
+    getUserDetails,
+    markNotificationsRead
+} = require('./handlers/users');
+
+// Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
+app.delete('/scream/:screamId', FBAuth, deleteScream);
+app.get('/scream/:screamId/like', FBAuth, likeScream);
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 // users routes
@@ -18,14 +40,10 @@ app.post('/signup', signup);
 app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
-app.delete('/scream/:screamId', FBAuth, deleteScream);
-app.get('/scream/:screamId/like', FBAuth, likeScream);
-app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
 app.get('/user', FBAuth, getAuthenticatedUser);
 app.get('/user/:handle', getUserDetails);
 app.post('/notifications', FBAuth, markNotificationsRead);
 
-// https://baseurl.com/api/
 exports.api = functions.region('europe-west1').https.onRequest(app);
 
 exports.createNotificationOnLike = functions
@@ -52,8 +70,6 @@ exports.createNotificationOnLike = functions
             })
             .catch((err) => console.error(err));
     });
-
-// deleteNotificationOnUnLike
 exports.deleteNotificationOnUnLike = functions
     .region('europe-west1')
     .firestore.document('likes/{id}')
@@ -66,7 +82,6 @@ exports.deleteNotificationOnUnLike = functions
                 return;
             });
     });
-
 exports.createNotificationOnComment = functions
     .region('europe-west1')
     .firestore.document('comments/{id}')
@@ -93,4 +108,64 @@ exports.createNotificationOnComment = functions
                 console.error(err);
                 return;
             });
+    });
+
+exports.onUserImageChange = functions
+    .region('europe-west1')
+    .firestore.document('/users/{userId}')
+    .onUpdate((change) => {
+        console.log(change.before.data());
+        console.log(change.after.data());
+        if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+            console.log('image has changed');
+            const batch = db.batch();
+            return db
+                .collection('screams')
+                .where('userHandle', '==', change.before.data().handle)
+                .get()
+                .then((data) => {
+                    data.forEach((doc) => {
+                        const scream = db.doc(`/screams/${doc.id}`);
+                        batch.update(scream, { userImage: change.after.data().imageUrl });
+                    });
+                    return batch.commit();
+                });
+        } else return true;
+    });
+
+exports.onScreamDelete = functions
+    .region('europe-west1')
+    .firestore.document('/screams/{screamId}')
+    .onDelete((snapshot, context) => {
+        const screamId = context.params.screamId;
+        const batch = db.batch();
+        return db
+            .collection('comments')
+            .where('screamId', '==', screamId)
+            .get()
+            .then((data) => {
+                data.forEach((doc) => {
+                    batch.delete(db.doc(`/comments/${doc.id}`));
+                });
+                return db
+                    .collection('likes')
+                    .where('screamId', '==', screamId)
+                    .get();
+            })
+            .then((data) => {
+                data.forEach((doc) => {
+                    batch.delete(db.doc(`/likes/${doc.id}`));
+                });
+                return db
+                    .collection('notifications')
+                    .where('screamId', '==', screamId)
+                    .get();
+            })
+            .then((data) => {
+                data.forEach((doc) => {
+                    batch.delete(db.doc(`/notifications/${doc.id}`));
+                });
+                return batch.commit();
+            })
+            .catch((err) => console.error(err));
     });
