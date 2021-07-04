3c61e0b8fae9d73a2127bfb0b016f75478026c34
Ajaya Rai <ar17052@essex.ac.uk>
2021-07-04 09:29:53 +0100

    Replace the text box and submit button with add icon and pop-up for adding Groups

--- final_product/bro-online-client/src/pages/home.js
+++ final_product/bro-online-client/src/pages/home.js
@@ -1,15 +1,22 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import axios from "axios";
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
-import AddInterest from "../components/AddInterest";
 import { Redirect } from 'react-router-dom';
 import Navbar from "../components/Navbar";
 import RmvGroupBtn from "../components/RmvGroupBtn";
 import Image from "../util/homePage_wallpaper.jpg";
+import MyButton from '../util/MyButton';
 
 // MUI stuff
 import withStyles from '@material-ui/core/styles/withStyles';
+import AddIcon from '@material-ui/icons/Add';
+import {Dialog} from "@material-ui/core";
+import CloseIcon from '@material-ui/icons/Close';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const styles = (theme) => ({
     groupBackground: {
@@ -20,12 +27,25 @@ const styles = (theme) => ({
             left: '91%',
             top: '6%'
         }
-    }
+    },
+    submitButton: {
+        position: 'relative',
+        float: 'right',
+        marginTop: 10
+    },
+    closeButton: {
+        position: 'absolute',
+        left: '91%',
+        top: '6%'
+    },
+    ...theme.spreadThis
 })
 
 class Home extends Component {
     state = {
         groups: [],
+        open: false,
+        body: ''
     }
 
     componentDidMount() {
@@ -38,6 +58,30 @@ class Home extends Component {
             }).catch((err) => {
             console.error(err);
         })
+    }
+
+    handleOpen = () => {
+        this.setState({open: true});
+    }
+
+    handleClose = () => {
+        this.setState({open: false});
+    }
+
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value});
+    };
+
+    handleSubmit = (event) => {
+        const x = {
+            name: this.state.body
+        }
+
+        axios
+            .post('/add_interest', x)
+            .catch((err) => {
+                console.error(err);
+            })
     }
 
     render() {
@@ -75,8 +119,51 @@ class Home extends Component {
                 <div className={classes.groupBackground}>
                     <h1>{`My Groups`}</h1>
                     {grpsFormattedInHTML.length !== 0 ? grpsFormattedInHTML : null}
-                    <AddInterest />
                 </div>
+
+                <Fragment>
+                    <MyButton onClick={this.handleOpen} tip="Add a group">
+                        <AddIcon/>
+                    </MyButton>
+                    <Dialog
+                        open={this.state.open}
+                        onCLose={this.handleClose}
+                        fullWith
+                        maxWith={`sm`}
+                    >
+                        <MyButton
+                            tip="Close"
+                            onClick={this.handleClose}
+                            tipClassName={classes.closeButton}
+                        >
+                            <CloseIcon/>
+                        </MyButton>
+                        <DialogTitle>Add a group</DialogTitle>
+                        <DialogContent>
+                            <form onSubmit={this.handleSubmit}>
+                                <TextField
+                                    name="body"
+                                    type="text"
+                                    label="Group Name"
+                                    multiline
+                                    rows="3"
+                                    placeholder="Name your group"
+                                    className={classes.textField}
+                                    onChange={this.handleChange}
+                                    fullWidth
+                                />
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.submitButton}
+                                >
+                                    Submit
+                                </Button>
+                            </form>
+                        </DialogContent>
+                    </Dialog>
+                </Fragment>
             </>
         );
     }
