c5a0bcc6854e281a95251c5b684cd968e8592167
Ajaya Rai <ar17052@essex.ac.uk>
2021-07-26 18:30:14 +0100

    Refactor ImageUpload on the backend

--- final_product/bro-online-functions/functions/index.js
+++ final_product/bro-online-functions/functions/index.js
@@ -1,14 +1,14 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
-const {admin, db} = require('./util/admin');
-const config = require('./util/config');
+const {db} = require('./util/admin');
 
 const {
     signup,
     login,
     getUsers,
-    getUserDetails
+    getUserDetails,
+    uploadImage
 } = require('./handlers/users');
 
 const {
@@ -28,54 +28,7 @@ app.post('/login', login);
 
 app.get('/user', FBAuth, getUserDetails)
 app.get('/users', FBAuth, getUsers);
-app.post('/user/image', FBAuth, (req, res) => {
-    const BusBoy = require('busboy');
-    const path = require('path');
-    const os = require('os');
-    const fs = require('fs');
-
-    const busboy = new BusBoy({headers: req.headers});
-
-    let imageFileName;
-    let imageToBeUploaded = {};
-
-    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-        if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
-            return res.status(400).json({error: 'Wrong file type submitted'});
-        }
-        // my.image.png
-        const imageExtension = filename.split('.')[filename.split('.').length - 1];
-        // 634345245245.png
-        imageFileName = `${Math.round(Math.random()*100000000000)}.${imageExtension}`;
-        const filepath = path.join(os.tmpdir(), imageFileName);
-        imageToBeUploaded = {filepath, mimetype};
-        file.pipe(fs.createWriteStream(filepath));
-    });
-
-    busboy.on('finish', () => {
-        admin.storage().bucket().upload(imageToBeUploaded.filepath, {
-            resumable: false,
-            metadata: {
-                metadata: {
-                    contentType: imageToBeUploaded.mimetype
-                }
-            }
-        })
-            .then(() => {
-                const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
-
-                return db.doc(`/users/${req.user.userName}`).update({imageUrl});
-            })
-            .then(() => {
-                return res.json({message: 'Image uploaded successfully'});
-            })
-            .catch(err => {
-                console.error(err);
-                return res.status(500).json({error: err.code});
-            })
-    })
-    busboy.end(req.rawBody);
-})
+app.post('/user/image', FBAuth, uploadImage);
 
 app.get('/get_my_grps', FBAuth, getUserGroups)
 app.post('/add_interest', FBAuth, addGroup);
